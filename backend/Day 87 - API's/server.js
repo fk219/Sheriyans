f4c88a58e0a18d455424ecbe100f4a5820d64d70
furqank219 <furqank219@gmail.com>
2026-04-22 14:29:52 +0000

    Day 89 - Done

--- backend/Day 87 - API's/server.js
+++ backend/Day 87 - API's/server.js
@@ -2,10 +2,17 @@ const express = require('express')
 
 const app = express();
 
+app.use(express.json())
 
 const notes = []
 
+app.get('/notes', (req, res)=>{
+    res.send(notes)
+})
+
 app.post('/notes', (req, res)=>{
+    console.log(req.body)
+    notes.push(req.body)
     res.send("Note Created Successfully!!")
 })
 
