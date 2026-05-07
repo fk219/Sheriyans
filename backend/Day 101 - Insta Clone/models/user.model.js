0bd273fdc014f97c657917744d03dd056b14fa44
furqank219 <furqank219@gmail.com>
2026-05-07 11:35:17 +0000

    Day 101 - CONTINUE

--- backend/Day 101 - Insta Clone/models/user.model.js
+++ backend/Day 101 - Insta Clone/models/user.model.js
@@ -1,15 +1,25 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongooseSchema({
-    username: String,
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: [true, "Username Already Exists"],
+        required: [true, "Username is Required"]
+    },
     email: {
         type: String,
-        unique: [true, "Duplicate Email Found!"]
+        unique: [true, "Duplicate Email Found!"],
+        required: [true, "Email is Required"]
     },
-    password: string,
+    password: {
+        type: String,
+        required: [true, "Password is Required"]
+    },
     bio: String,
-    followers: Array,
-    profile_image: String
+    profile_image: {
+        type: String,
+        default: "https://ik.imagekit.io/fk219/default_profile.png"
+    }
 })
 
 const userModel = mongoose.model("Users", userSchema)
--- backend/Day 101 - Insta Clone/routes/auth.route.js
+++ backend/Day 101 - Insta Clone/routes/auth.route.js
@@ -1,9 +1,70 @@
-const express = require('express')
+const express = require('express');
+const userModel = require('../models/user.model');
+const crypto = require('crypto')
+const jwt= require('jsonwebtoken')
 
 const authRouter = express.Router()
 
-authRouter.post('regitser', async (req, res) => {
-    
+authRouter.post('/regitser', async (req, res) => {
+    const {username, email, password, bio, profileImage} = req.body;
+
+    // const isUserExistsByEmail = await userModel.findOne({email})
+
+    // if(!isUserExistsByEmail){
+    //     res.status(409).json({
+    //         message: "User Already Exists with Same Email!" 
+    //     })
+    // }
+
+    // const isUserExistsByUsername = await userModel.findOne({username})
+
+    // if(!isUserExistsByUsername){
+    //     res.status(409).json({
+    //         message: "User Already Exists with Same Username!"
+    //     })
+    // }
+
+    const isUserAlreadyExists = await userModel.findOne({
+        $or:[
+            {email},
+            {username}
+        ]
+    }) 
+
+    if(isUserAlreadyExists){
+        res.status(409).json({
+            message: "User Already Exists with Same Email or Username!" + (isUserAlreadyExists.email === email ? "Email Aready Exists" : "Username Already Exists!")
+        })
+    }
+
+    const hashedPassword = crypto.createHash('md5').update(password).digest('hex')
+
+    const user = userModel.create({
+        username,
+        email,
+        password: hashedPassword,
+        bio,
+        profileImage
+    })
+
+    const token = jwt.sign({
+        id: user._id,
+    }, process.env.JWT_SECRET, {
+        expiresIn: "1d"
+    })
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message: "User Registered Successfully",
+        user: {
+            email: user.email,
+            username: (await user).username,
+            bio: (await user).bio,
+            profileImage: user.profileImage
+        }
+    })
+
 })
 
 authRouter.post('login', async (req, res) => {
