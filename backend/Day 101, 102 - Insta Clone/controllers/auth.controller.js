32f5d2fd3959bfaffef2b536d3c87e936c3ec13d
furqank219 <furqank219@gmail.com>
2026-05-08 07:36:01 +0000

    Day 102 - STARTED

---
+++ backend/Day 101, 102 - Insta Clone/controllers/auth.controller.js
@@ -0,0 +1,101 @@
+const userModel = require("../models/user.model");
+// const crypto = require("crypto");
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+
+const registerController = async (req, res) => {
+  const { username, email, password, bio, profileImage } = req.body;
+
+  const isUserAlreadyExists = await userModel.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if (isUserAlreadyExists) {
+    return res.status(409).json({
+      message:
+        "User Already Exists" +
+        (isUserAlreadyExists.email === email
+          ? "Email Already Exists"
+          : "Username Already Exists!"),
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const user = await userModel.create({
+    username,
+    email,
+    password: hashedPassword,
+    bio,
+    profileImage,
+  });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User Registered Successfully",
+    user: {
+      email: user.email,
+      username: user.username,
+      bio: user.bio,
+      profileImage: user.profile_image,
+    },
+  });
+};
+
+const loginController = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const user = await userModel.findOne({
+    $or: [{ username: username }, { email: email }],
+  });
+
+  if (!user) {
+    res.status(409).json({
+      message: "User is not found!",
+    });
+  }
+
+  const doesPasswordMatch = bcrypt.compare(password, user.password)
+
+  if (!doesPasswordMatch) {
+    res.status(401).json({
+      message: "Incorrect Password!!",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "User Logged In Successfully",
+    user: {
+      username: user.username,
+      email: user.email,
+      bio: user.bio,
+      profileImage: user.profile_image,
+    },
+  });
+};
+
+module.exports = {
+  registerController,
+  loginController,
+};
