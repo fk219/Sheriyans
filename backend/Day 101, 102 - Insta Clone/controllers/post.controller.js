2025a59af3cab7f4248f258d0b30fa8f531d11cd
furqank219 <furqank219@gmail.com>
2026-05-10 16:30:14 +0000

    Dqy 103 - CONTINUE

--- backend/Day 101, 102 - Insta Clone/controllers/post.controller.js
+++ backend/Day 101, 102 - Insta Clone/controllers/post.controller.js
@@ -1,4 +1,5 @@
 const postModel = require("../models/post.model")
+const jwt = require("jsonwebtoken")
 const ImageKit = require("@imagekit/nodejs")
 const { toFile } = require("@imagekit/nodejs")
 
@@ -7,16 +8,33 @@ const imagekitInstance = new ImageKit({
 })
 
 const createPostController = async (req, res) => {
-        console.log(req.body, req.file)
+        // console.log(req.body, req.file)
         
         const file = await imagekitInstance.files.upload({
             file: await toFile(req.file.buffer, req.file.originalname),
             fileName: req.file.originalname
         })
 
-        res.send({
-            file: file
+        const token = req.cookies.token;
+
+        if(!token){
+            res.status(401).json({
+                message: "Unauthorised Access!!"
+            })
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const post = await postModel.create({
+            caption: req.body.caption,
+            image_url: file.url,
+            user: decoded.id
         })
+
+        res.status(201).json({
+            message: "Post Created Successfully"
+        })
+
 }
 
 module.exports = {
