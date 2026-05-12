c558e2f0ff8eb522dae80d8db78dc969f2b68491
furqank219 <furqank219@gmail.com>
2026-05-12 09:54:55 +0000

    Day 104 - COMPLETED

--- backend/Day 101, 102 - Insta Clone/controllers/post.controller.js
+++ backend/Day 101, 102 - Insta Clone/controllers/post.controller.js
@@ -10,20 +10,28 @@ const imagekitInstance = new ImageKit({
 const createPostController = async (req, res) => {
         // console.log(req.body, req.file)
         
-        const file = await imagekitInstance.files.upload({
-            file: await toFile(req.file.buffer, req.file.originalname),
-            fileName: req.file.originalname
-        })
-
+       let decoded;
         const token = req.cookies.token;
 
         if(!token){
-            res.status(401).json({
-                message: "Unauthorised Access!!"
+            return res.status(401).json({
+                message: "Token Not Provided! Unauthorised Access!!"
             })
         }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        try{
+             decoded = jwt.verify(token, process.env.JWT_SECRET)
+        }catch (err){
+            return res.status(401).json({
+                message: "Unauthorised Access!"
+            })
+        }
+
+        const file = await imagekitInstance.files.upload({
+            file: await toFile(Buffer.from(req.file.buffer), req.file.originalname),
+            fileName: req.file.originalname,
+            folder: "insta-clone"
+        })
 
         const post = await postModel.create({
             caption: req.body.caption,
@@ -32,11 +40,85 @@ const createPostController = async (req, res) => {
         })
 
         res.status(201).json({
-            message: "Post Created Successfully"
+            message: "Post Created Successfully",
+            post
         })
-
 }
 
+const getPostController = async (req, res) => {
+    const token = req.cookies.token
+    
+    if(!token){
+        return res.status(409).json({
+            message: "Please Login First!"
+        })
+    }
+
+    let decoded = null;
+    try{
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({
+            message: "Unauthorised Access!"
+        })
+    }
+
+    const userId = decoded.id
+
+    const posts = await postModel.find({
+        user: userId
+    })
+    
+    res.status(201).json({
+        message: "All Posts Fetched Successfully",
+        posts
+    })
+}
+
+const getPostDetailsController = async (req, res) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorised Access! No Token Present!"
+        });
+    }
+
+    let decoded = null;
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        return res.status(401).json({
+            message: "Unauthorised Access"
+        });
+    }
+    
+    const userId = decoded.id;
+
+    const post = await postModel.findById(req.params.postId);
+
+    if (!post) {
+        return res.status(404).json({
+            message: "Post Not Found"
+        });
+    }
+
+    const isValidUser = post.user.toString() === userId;
+
+    if (!isValidUser) {
+        return res.status(403).json({
+            message: "Forbidden Content: You can only view your own posts."
+        });
+    }
+
+    res.status(200).json({
+        message: "Post Details Fetched Successfully",
+        post
+    });
+};
+
+
 module.exports = {
-    createPostController
+    createPostController, 
+    getPostController,
+    getPostDetailsController
 }
--- backend/Day 101, 102 - Insta Clone/routes/post.route.js
+++ backend/Day 101, 102 - Insta Clone/routes/post.route.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {createPostController} = require("../controllers/post.controller")
+const {createPostController, getPostController, getPostDetailsController} = require("../controllers/post.controller")
 
 const multer = require("multer")
 const upload = multer({storage:multer.memoryStorage()})
@@ -7,5 +7,7 @@ const upload = multer({storage:multer.memoryStorage()})
 const postRouter = express.Router()
 
 postRouter.post('/', upload.single('image'), createPostController)
+postRouter.get('/', getPostController)
+postRouter.post('/details/:postId', getPostDetailsController)
 
 module.exports = postRouter
