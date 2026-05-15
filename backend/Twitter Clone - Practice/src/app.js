2f8d4a2b9af14e88bf15bb646b02b25d33ce9550
furqank219 <furqank219@gmail.com>
2026-05-15 14:48:25 +0000

    Day 107 - STARTED

---
+++ backend/Twitter Clone - Practice/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const cookieParser = require("cookie-parser")
+
+
+const authRouter = require("../routes/auth.route")
+const postRouter = require("../routes/post.route")
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth", authRouter)
+app.use("/api/post", postRouter)
+
+app.get("/", (req, res) => {
+    res.send("APU is Up and Running!")
+})
+
+module.exports= app
