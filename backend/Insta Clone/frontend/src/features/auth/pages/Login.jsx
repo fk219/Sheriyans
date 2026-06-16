69437c6346b9e0e5cc6c7aecf76982c184a78865
Furqan Khan Ahmed Irfan <97156473+fk219@users.noreply.github.com>
2026-06-16 11:58:25 +0000

    Day 108 - COMPLETED | Setted Up Browser Router, Created Two Page: Register and Login. Folloing feature Base Arhcitecture. Integrated Backend with Register And Login

--- backend/Insta Clone/frontend/src/features/auth/pages/Login.jsx
+++ backend/Insta Clone/frontend/src/features/auth/pages/Login.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/form.scss'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 // https://insta-clone-backend-n518.onrender.com/
 
 const Login = () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    axios.post('https://insta-clone-backend-n518.onrender.com/api/auth/login', {
+      username,
+      password
+    }, {
+      credentials: true
+    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }
+  
   return (
     <main>
         <div className="form-container">
             <h1>Login</h1>
-            <form className="form" action="">
-                <input type="text" name="username" placeholder='Username' />
-                <input type="password" name="password" placeholder='Password' />
+            <form className="form" onSubmit={handleSubmit}>
+                <input 
+                  type="text" 
+                  name="username" 
+                  placeholder='Username'
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <input 
+                  type="password" 
+                  name="password" 
+                  placeholder='Password'
+                  onChange={(e) => setPassword(e.target.value)}
+                />
                 <button type="submit">Login</button>
             </form>
             <p>Don't have an account? <Link className="toggleAuthForm" to="/register">Register</Link></p>
