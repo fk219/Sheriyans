b7338e64a575e32e96ef87dd9dfd15c45fef2db4
furqank219 <furqank219@gmail.com>
2026-07-02 14:30:20 +0000

    Day 112 - STARTED

---
+++ backend/Moodify/src/App.jsx
@@ -0,0 +1,12 @@
+import FaceExpression from '../src/features/faceExpression/page/FaceExpression'
+
+function App() {
+
+  return (
+    <>
+      <FaceExpression />
+    </>
+  )
+}
+
+export default App
