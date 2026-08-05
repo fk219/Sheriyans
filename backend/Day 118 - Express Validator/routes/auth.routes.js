55705c43db9aa438f2990a753ac724f7b6910881
Furqan Khan Ahmed Irfan <97156473+fk219@users.noreply.github.com>
2026-08-05 15:07:07 +0000

    Day 118 - CONTINUE

--- backend/Day 118 - Express Validator/routes/auth.routes.js
+++ backend/Day 118 - Express Validator/routes/auth.routes.js
@@ -1,28 +1,13 @@
 import express from 'express'
 import {registerController} from '../controllers/auth.controller.js'
-import {body, validationResult} from 'express-validator'
+import { registerValidation } from '../validation/auth.validator.js'
 
 
 
 const authRouter = express.Router()
 
 
-authRouter.post('/register',[
-    body('username').isString().withMessage("Username Should be a String Bastard"),
-    body('email').isEmail().withMessage("Email Daal gandu"),
-
-    (req, res, next) => {
-        const errors = validationResult(req)
-
-        if(errors.isEmpty()){
-            return next()
-        }
-
-        res.status(400).json({
-            errors: errors.array()
-        })
-    }
-],registerController)
+authRouter.post('/register', registerValidation, registerController)
 
 
 export {authRouter}
