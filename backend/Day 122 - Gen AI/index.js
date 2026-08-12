1ae651b5e4404f2be88aa010695996207aefb0a4
Furqan Khan Ahmed Irfan <97156473+fk219@users.noreply.github.com>
2026-08-12 12:50:19 +0000

    Day 122 - COMPLETED

--- backend/Day 122 - Gen AI/index.js
+++ backend/Day 122 - Gen AI/index.js
@@ -1,14 +1,35 @@
-import readline from 'readline'
+import "dotenv/config"
+import readline from 'readline/promises'
+import { ChatMistralAI } from "@langchain/mistralai";
+import {HumanMessage} from 'langchain'
+
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-
-rl.question("Whats is Your Name? ", (name) => {
-    console.log(`Hello ${name}`)
-    rl.close()
+const model = new ChatMistralAI({
+    model: 'mistral-small-latest',
+    temperature: 0
 })
 
+// rl.question("Hi How can I help you?", async (answer)=> {
+//     const response = await model.invoke(answer)
+//     console.log(response.text)
+// })
 
+
+const messages = []
+
+while(true){
+    const userInput = await rl.question("You: ")
+
+    messages.push(new HumanMessage(userInput))
+
+    const response = await model.invoke(messages)
+
+    messages.push(response)
+
+    console.log("AI:", response.text)
+}
