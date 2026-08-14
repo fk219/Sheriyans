fec2ec1a10bc8dac4746111894a8c9e5d9701c37
Furqan Khan Ahmed Irfan <97156473+fk219@users.noreply.github.com>
2026-08-14 13:38:08 +0000

    Day 124 - CONTINUE (Socket on Server)

--- backend/Perplexity/backend/server.js
+++ backend/Perplexity/backend/server.js
@@ -1,11 +1,22 @@
 import 'dotenv/config';
 
+import http from "http"
 import { app } from './src/app.js';
 import { connectDB } from './src/config/database.js';
+import { socketInit } from './src/sockets/server.socket.js';
 
 const port = process.env.PORT || 3000;
 
-connectDB();
-app.listen(port, () => {
+const httpServer = http.createServer(app)
+
+socketInit(httpServer)
+
+connectDB()
+.catch((err)=>{
+    console.error("MongoDB Connection Failed! ", err)
+    process.exit(1)
+});
+
+httpServer.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);
 });
---
+++ backend/Perplexity/backend/src/sockets/server.socket.js
@@ -0,0 +1,28 @@
+import {Server} from "socket.io"
+
+let io;
+
+const socketInit = (httpServer) => {
+    io = new Server(httpServer, {
+        cors: {
+            origin: ["http://localhost:5173", "https://scaling-waddle-q5v7p6gvxj73wrx-5173.app.github.dev/"],
+            credentials: true
+        }
+    })
+
+    console.log("SocketIO Server is Running!")
+
+    io.on('connection', (socket) => {
+        console.log("A user is connected: "+ socket.id)
+    })
+}
+
+const getIO = () => {
+    if(!io){
+        throw new Error("Socker IO is Not Initialized")
+    }
+
+    return io
+}
+
+export {socketInit, getIO}
