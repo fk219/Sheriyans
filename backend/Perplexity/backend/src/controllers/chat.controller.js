8cb4558fa4c7650aae04173a54ed4f25b1f35ce4
fk219 <furqank219@gmail.com>
2026-08-15 20:34:31 +0530

    Day 125 - COMPLETED

--- backend/Perplexity/backend/src/controllers/chat.controller.js
+++ backend/Perplexity/backend/src/controllers/chat.controller.js
@@ -3,7 +3,7 @@ import { generateTitle } from '../services/ai.services.js'
 import {chatModel} from '../models/chat.model.js'
 import {messageModel} from '../models/message.model.js'
 
-const messageController = async (req, res) => {
+const sendMessages = async (req, res) => {
     try{
         const {message, chat: chatId} = req.body
         let title = null, newChat = null;
@@ -47,4 +47,71 @@ const messageController = async (req, res) => {
     }
 }
 
-export {messageController}
+const getChats = async (req, res) => {
+    const user = req.user
+
+    const chats = await chatModel.find({user: user.id})
+
+    res.status(200).json({
+        message: "Chats Fetched Succeesully",
+        chats: chats
+    })
+}
+
+const getMessages = async (req, res) => {
+    const {chatId} = req.params
+
+    const chat = await chatModel.findOne({
+        user: req.user._id,
+        _id: chatId
+    })
+
+    if(!chat){
+        return res.status(404).json({
+            success: false,
+            message: "Chat Not Found"
+        })
+    }
+
+
+    const messages = await messageModel.find({
+        chat: chatId
+    })
+
+    return res.status(200).json({
+        message: "Messages Fetched Succeesully",
+        success: true,
+        messages: messages
+    })
+}
+
+const deleteChats = async (req, res) => {
+    const userId = req.user.id
+    const {chatId} = req.params
+
+    const chat = await chatModel.findOneAndDelete({
+        _id: chatId,
+        user: userId
+    })
+
+    if(!chat){
+        return res.status(404).json({
+            message: "Chat Not Found",
+            success: false
+        })
+    }
+
+    await messageModel.deleteMany({
+        chat: chatId
+    })
+
+    res.status(200).json({
+        message: "Chat Delleted Successfuly!",
+        success: true,
+        chat: chatId
+    })
+
+    
+}
+
+export {sendMessages, getChats, getMessages, deleteChats}
--- backend/Perplexity/backend/src/routes/chat.routes.js
+++ backend/Perplexity/backend/src/routes/chat.routes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { identifyUser } from '../middleware/auth.middleware.js'
-import {messageController} from '../controllers/chat.controller.js'
+import {sendMessages, getChats, getMessages, deleteChat} from '../controllers/chat.controller.js'
 
 
 const chatRouter = express.Router()
 
-chatRouter.post('/message', identifyUser, messageController)
-
+chatRouter.post('/message', identifyUser, sendMessages)
+chatRouter.get('/', identifyUser, getChats)
+chatRouter.get('/:chatId/messages', identifyUser, getMessages)
+chatRouter.delete('/delete/:chatId', identifyUser, deleteChat)
 
 export default chatRouter
