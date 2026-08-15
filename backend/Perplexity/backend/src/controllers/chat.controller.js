e93fc375ee4ec0a1765e05a369cf0741bfc9a68f
fk219 <furqank219@gmail.com>
2026-08-15 19:32:18 +0530

    Day 125 - CONTINUE

--- backend/Perplexity/backend/src/controllers/chat.controller.js
+++ backend/Perplexity/backend/src/controllers/chat.controller.js
@@ -1,15 +1,47 @@
 import {generateResponse} from '../services/ai.services.js'
+import { generateTitle } from '../services/ai.services.js'
+import {chatModel} from '../models/chat.model.js'
+import {messageModel} from '../models/message.model.js'
 
 const messageController = async (req, res) => {
     try{
-        const {message} = req.body
+        const {message, chat: chatId} = req.body
+        let title = null, newChat = null;
         
-        const result = await generateResponse(message)
-    
-        res.status(201).json({
-            message: result
+        if(!chatId){
+            title = await generateTitle(message)
+
+            newChat = await chatModel.create({
+                title: title,
+                user: req.user.id
+            })
+        }
+
+        const currentChatId = chatId || newChat._id 
+        
+        const userMessage = await messageModel.create({
+            chat: currentChatId,
+            content: message,
+            role: "user"
         })
         
+        const chatMessages = await messageModel.find({chat: currentChatId}).sort({createdAt: 1})
+        
+        const result = await generateResponse(chatMessages)
+
+        const aiMessage = await messageModel.create({
+            chat: currentChatId,
+            content: result,
+            role: "ai"
+        })
+
+        res.status(201).json({
+            title: title,
+            newChat: newChat,
+            userMessage: userMessage,
+            aiMessage: aiMessage
+        })
+
     }catch(error){
         res.status(500).json({error: error.message})
     }
--- backend/Perplexity/backend/src/services/ai.services.js
+++ backend/Perplexity/backend/src/services/ai.services.js
@@ -1,16 +1,37 @@
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage } from 'langchain'
+import { HumanMessage, AIMessage, SystemMessage } from 'langchain'
 
 const model = new ChatMistralAI({
   model: "mistral-small-latest"
 });
 
-const generateResponse = async (message) => {
-  const response = await model.invoke([
-    new HumanMessage(message)
-  ])
+const generateResponse = async (messages) => {
+  const response = await model.invoke(messages.map((msg) => {
+    if(msg.role === "user"){
+      return new HumanMessage(msg.content)
+    }else if(msg.role === "ai"){
+      return new AIMessage(msg.content)
+    }
+  }))
 
   return response.text
 }
 
-export {generateResponse}
+
+const generateTitle = async (message) => {
+  const title = await model.invoke([
+    new SystemMessage(`
+      You are a helpful AI assistance your task is to create a title for the user's query. 
+      The title should be short and concise and should be relevant to the user's query.
+      The title should be in the same language as the user's query.
+      Do not use any special characters or symbols in the title. 
+      Do not use any quotes or punctuation in the title.
+      `),
+    new HumanMessage(`
+        Generate a title in 2-4 words for the following message: ${message}
+        `)
+  ])
+  return title.text
+}
+
+export { generateResponse, generateTitle }
