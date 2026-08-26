6f2803af6b240031a14d7b6eb658d367c5cff346
furqank219 <furqank219@gmail.com>
2026-08-26 08:57:19 +0000

    TYPESCRIPT - STARTED

--- backend/Day 88 - REST API/server.js
+++
@@ -1,5 +0,0 @@
-const app = require("./src/app");
-
-app.listen(4000, () => {
-  console.log(`Your Server is Running at http://localhost:4000`);
-});
--- backend/Day 89 - Database/server.js
+++
@@ -1,6 +0,0 @@
-const app = require("./src/app");
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Server is Running at http://localhost:3000`);
-});
