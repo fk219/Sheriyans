f67faf5693035c9215917ab3729641d005d7f8b6
Furqan Khan Ahmed Irfan <97156473+fk219@users.noreply.github.com>
2026-08-27 15:00:13 +0000

    Typescript - 01.22.12

--- Typescript Domination/4_Into to Interfaces & Type Aliases/app.ts
+++ Typescript Domination/4_Into to Interfaces & Type Aliases/app.ts
@@ -4,23 +4,83 @@
 // INterface === Shakal
 //  In the below example "useProfile" ki shakal "User" jaisi hai
 
-interface User {
-    name: string,
-    email: string
-    age: number,
-    isMale: boolean
-}
+// interface User {
+//     name: string,
+//     email: string
+//     age: number,
+//     isMale: boolean
+//     gender?: string
+// }
 
-function sample(userProfile: User): void{
-    userProfile.age = 12,
-    userProfile.email = "fk@fk.com",
-    userProfile.name = 'Furqan'
-}
-
-
-
+// function sample(userProfile: User): void{
+//     userProfile.age = 12,
+//     userProfile.email = "fk@fk.com",
+//     userProfile.name = 'Furqan'
+// }
 
 
 
+// 2 interfaces of same number will be merged automaticaly
+// interface abc {
+//     email: string
+// }
 
+// interface abc {
+//     phone: number
+// }
+
+
+
+// --------------------EXTENDING INTERFACE
+// Admin ke paas user ki saari properties hogi with an extra prop. admin: bolean
+
+// interface User {
+//     name: string,
+//     age: number,
+//     gender?: string,
+//     isMale: boolean
+// }
+
+// interface Admin extends User {
+//     admin: boolean
+// }
+
+
+
+// ----------------------------- TYPE ALIASES ----------------------------
 // Types ka kaam hai User defined type banana
+// Apne man chahe naam ka type bana sakte ho
+// Khudke ka type banake ghoshit kr skte ho
+
+// Example
+// type sankhya = number;
+// let a: sankhya
+
+// Actual usecase
+// type value = string | number | null
+// let a: value
+
+
+
+// ---------------------Union & Intersection Types
+
+// Union "|" (Ya to stroing ya to null)
+// let a: string | null
+
+// INtersection '&'
+
+type User = {
+    email: string,
+    name: string
+}
+
+type Admin = User & {
+    getDetails(user: string): void
+}
+
+
+// Diff between type and interface
+//  In types below example will throw an error, interface me merge kr rha tha 
+// type abc = string
+// type abc = number
+
