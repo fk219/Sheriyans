bd7a44f4187e18edd41d97fb2da2fdf1bf5dd8dd
Furqan Khan Ahmed Irfan <97156473+fk219@users.noreply.github.com>
2026-08-27 09:15:42 +0000

    Typescript - 37.28

---
+++ Typescript Domination/2_Intro to Basic Types (Primitive Tpes, Arrays, Tuples, Enums, Any, Unknown, Void, Undefined, Never)/app.ts
@@ -0,0 +1,43 @@
+// Primitives & Reference
+
+
+// ------------------- REFERENCE
+// [] {} ()
+// Jo datatype me brackets lage wo reference data type hai 
+// Inme koi bhi changes hogi to parent me reflect hoga
+// IN the below example 'b' ki koi value hi nhi hai wo sirf 'a' ki value ka reference hai
+
+// Example
+// let a = [1, 2, 3, 4, 5];
+// let b = a;
+// b.pop();
+// console.log(a);
+
+// Answer: [1,2,3,4]
+
+// Primitive ko app directly copy kr skte ho a=b pr reference ko direct copy nhi krskte
+
+
+// ----------------- PRIMITIVE
+// STRINGS, NUMBERS, BOOLEAN are primitive datatypes
+
+// Defining Variable:
+// var (dont use)
+// let
+// const
+
+// Number:
+// let a = 12
+
+// String
+// let a = 'coding'
+
+// Boolean
+// let a = true
+
+
+// --------------------ARRAYS
+// let arr = [1,2,3]
+
+
+// 
