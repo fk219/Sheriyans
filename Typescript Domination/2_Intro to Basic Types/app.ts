4f4b316cd45457a7386a8b815835c72aca1e8a9a
Furqan Khan Ahmed Irfan <97156473+fk219@users.noreply.github.com>
2026-08-27 09:07:20 +0000

    TYPESCRIPT - 31.14

---
+++ Typescript Domination/2_Intro to Basic Types/app.ts
@@ -0,0 +1,21 @@
+// Primitives & Reference
+
+// PRIMITIVE
+// STRINGS, NUMBERS, BOOLEAN are primitive datatypes
+
+// REFERENCE
+// [] {} ()
+// Jo datatype me brackets lage wo reference data type hai 
+// Inme koi bhi changes hogi to parent me reflect hoga
+// IN the below example 'b' ki koi value hi nhi hai wo sirf 'a' ki value ka reference hai
+
+// Example
+// let a = [1, 2, 3, 4, 5];
+// let b = a;
+// b.pop();
+// console.log(a);
+
+// Answer: [1,2,3,4]
+
+
+// Primitive ko app directly copy kr skte ho a=b pr reference ko direct copy nhi krskte
