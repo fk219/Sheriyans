17323099ad5582996febe05e5394556d1d42399b
Furqan Khan Ahmed Irfan <97156473+fk219@users.noreply.github.com>
2026-09-01 14:38:49 +0000

    Typescript- 01.54.54

---
+++ Typescript Domination/5_Intro to Classes & Objects/app.ts
@@ -0,0 +1,24 @@
+// Two ways for defining classes
+
+// // 1st Way
+// class Human {
+//     public name
+//     public sex
+//     public age
+//     constructor(name: string, sex: string, age: number){
+//         this.name = name
+//         this.sex = sex
+//         this.age = originAgentCluster
+//     }
+// }
+
+// // 2nd Way
+// class createHuman {
+//     constructor(public name: string, public sex: string, public age: number){
+
+//     }
+// }
+
+// THIS KEYWORD
+
+
